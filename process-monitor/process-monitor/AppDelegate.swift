c16df98abebaaf979a96554216c08d2f2352d825
Roman Kopaliani <romankopaliani@pingidentity.com>
2021-02-03 15:34:00 +0200

    Project creation

---
+++ process-monitor/process-monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  process-monitor
+//
+//  Created by Roman Kopaliani on 03.02.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ process-monitor/process-monitor/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  process-monitor
+//
+//  Created by Roman Kopaliani on 03.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
