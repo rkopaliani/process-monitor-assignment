e71d2174fad9f325e9ce07d5e5fe214655e2efa0
Roman Kopaliani <romankopaliani@pingidentity.com>
2021-02-09 13:18:13 +0200

    Add calls for sysctl to fetch pid and uid

--- process-monitor/process-monitor/Source/Process.swift
+++ process-monitor/process-monitor/Source/Process.swift
@@ -22,21 +22,44 @@ extension ProcessInfo: Hashable {}
 
 extension ProcessInfo {
     init?(_ app: NSRunningApplication) {
-        var kinfo = kinfo_proc()
-        var size  = MemoryLayout<kinfo_proc>.stride
-        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, app.processIdentifier]
-        
-        guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
-            fatalError("Something went wrong. Abort. Check this later")
+        guard let ppid = fetchPid(for: app.processIdentifier),
+              let uid = fetchUid(for: app.processIdentifier) else {
             return nil
         }
-
         self.pid = app.processIdentifier
-        self.ppid = kinfo.kp_eproc.e_ppid
-        self.uid = kinfo.kp_eproc.e_ucred.cr_uid
+        self.ppid = ppid
+        self.uid = uid
 
         self.bundleId = app.bundleIdentifier
         self.path = app.bundleURL?.absoluteString ?? ""
         self.certificateTeamId = "cert"
     }
 }
+
+func fetchPid(for pid: pid_t) -> pid_t? {
+    var kinfo = kinfo_proc()
+    var size  = MemoryLayout<kinfo_proc>.stride
+    var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
+    
+    guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
+        print("Something went wrong in sysctl on parentPid.")
+        return nil
+    }
+    
+    return kinfo.kp_eproc.e_ppid
+}
+
+func fetchUid(for pid: pid_t) -> uid_t? {
+    var kinfo = kinfo_proc()
+    var size  = MemoryLayout<kinfo_proc>.stride
+    var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
+    
+    guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
+        print("Something went wrong in sysctl on uid.")
+        return nil
+    }
+    
+    return kinfo.kp_eproc.e_ucred.cr_uid
+}
+
+
