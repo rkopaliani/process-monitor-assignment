f52c21a80462da5a6ac8df111a0056c72b19c075
Roman Kopaliani <romankopaliani@pingidentity.com>
2021-02-10 17:43:52 +0200

    Clean up and comments

--- process-monitor/process-monitor/Source/Process.swift
+++ process-monitor/process-monitor/Source/Process.swift
@@ -22,14 +22,13 @@ struct ProcessData {
     var signingInfo: SigningInfo?
 }
 
+
 extension ProcessData: Hashable {}
 
 extension ProcessData {
     init?(_ app: NSRunningApplication) {
-        
-        guard let ppid = fetchPid(for: app.processIdentifier),
-              let uid = fetchUid(for: app.processIdentifier) else {
-            print("bail")
+        guard let ppid = ProcessDataFetcher.fetchPid(for: app.processIdentifier),
+              let uid = ProcessDataFetcher.fetchUid(for: app.processIdentifier) else {
             return nil
         }
         
@@ -39,39 +38,11 @@ extension ProcessData {
         self.bundleId = app.bundleIdentifier
         self.path = app.bundleURL?.absoluteString ?? ""
         self.certificateTeamId = "cert"
-        self.signingInfo = extractCodeSigningInfo(for: self.pid)
-        print("ProcessID \(pid), user \(self.uid) bunldeId \(String(describing: self.bundleId)), path\(self.path)")
+        self.signingInfo = CodeSigningInfoExtractor.extract(for: self.pid)
     }
 }
 
-
-func fetchPid(for pid: pid_t) -> pid_t? {
-    var kinfo = kinfo_proc()
-    var size  = MemoryLayout<kinfo_proc>.stride
-    var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
-    
-    guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
-        print("Something went wrong in sysctl on parentPid.")
-        return nil
-    }
-    
-    return kinfo.kp_eproc.e_ppid
-}
-
-func fetchUid(for pid: pid_t) -> uid_t? {
-    var kinfo = kinfo_proc()
-    var size  = MemoryLayout<kinfo_proc>.stride
-    var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
-    
-    guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
-        print("Something went wrong in sysctl on uid.")
-        return nil
-    }
-    
-    return kinfo.kp_eproc.e_ucred.cr_uid
-}
-
-enum CodeSinger {
+enum CodeSigner {
     case apple
     case appstore
     case developer
@@ -79,69 +50,121 @@ enum CodeSinger {
 }
 
 struct SigningInfo: Hashable {
-    let signingStatus: OSStatus
-    let codeSigner: CodeSinger
+    let codeSigner: CodeSigner
+    var teamIdentifier: String?
+    var signingIdentifier: String?
 }
 
-func extractCodeSigningInfo(for pid: pid_t) -> SigningInfo? {
-    var status: OSStatus = -1
-    var dynamicCode: SecCode?
-    status = SecCodeCopyGuestWithAttributes(nil,
-                                            [kSecGuestAttributePid : pid] as CFDictionary,
-                                            SecCSFlags([]),
-                                            &dynamicCode)
-    guard status == errSecSuccess else { return nil }
-    guard let unwrappedDynamicCode = dynamicCode else { return nil }
-    
-    status = SecCodeCheckValidity(unwrappedDynamicCode, SecCSFlags([]), nil)
-    guard status == errSecSuccess else { return nil }
-    
-    let codeSigner = extractCodeSigner(unwrappedDynamicCode, flags: SecCSFlags([]))
-    
-    var signingInfo: CFDictionary?
-    var staticCode: SecStaticCode?
-    status = SecCodeCopyStaticCode(unwrappedDynamicCode, SecCSFlags([]), &staticCode)
-    guard status == errSecSuccess else { return nil }
-    guard let unwrappedStaticCode = staticCode else { return nil }
 
-    status = SecCodeCopySigningInformation(unwrappedStaticCode,
-                                           SecCSFlags(rawValue: kSecCSSigningInformation),
-                                           &signingInfo)
-    guard status == errSecSuccess else { return nil }
-    guard let castedSigningInfo = signingInfo as? Dictionary<AnyHashable, Any> else { return nil }
-    
-    if let teamIdentifier = castedSigningInfo[kSecCodeInfoTeamIdentifier] as? String {
-        print(teamIdentifier)
+fileprivate struct ProcessDataFetcher {
+    //TODO: not the best idea to call sysctl two times for no reason at all. Return tuple with pid and uid.
+    static func fetchPid(for pid: pid_t) -> pid_t? {
+        var kinfo = kinfo_proc()
+        var size  = MemoryLayout<kinfo_proc>.stride
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
+        
+        guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
+            print("Something went wrong in sysctl on parentPid.")
+            return nil
+        }
+        
+        return kinfo.kp_eproc.e_ppid
     }
 
-    if let dentifier = castedSigningInfo[kSecCodeInfoIdentifier] as? String {
-        print(dentifier)
+    static func fetchUid(for pid: pid_t) -> uid_t? {
+        var kinfo = kinfo_proc()
+        var size  = MemoryLayout<kinfo_proc>.stride
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
+        
+        guard sysctl(&mib, u_int(mib.count), &kinfo, &size, nil, 0) == 0 else {
+            print("Something went wrong in sysctl on uid.")
+            return nil
+        }
+        
+        return kinfo.kp_eproc.e_ucred.cr_uid
     }
-
-    return SigningInfo(signingStatus: status, codeSigner: codeSigner)
 }
 
-func extractCodeSigner(_ code: SecCode, flags: SecCSFlags) -> CodeSinger {
-    var isApple: SecRequirement?
-    var isAppStore: SecRequirement?
-    var isDeveloper: SecRequirement?
+
+fileprivate struct CodeSigningInfoExtractor {
     
-    _ = SecRequirementCreateWithString("anchor apple" as CFString,
+    //TODO: it's not thread safe, though thread-safety better be ensured on a callers site
+    static var appleSecRequirement: SecRequirement? = {
+        var isApple: SecRequirement?
+        SecRequirementCreateWithString("anchor apple" as CFString,
                                        SecCSFlags(rawValue: 0),
                                        &isApple)
-    _ = SecRequirementCreateWithString("anchor apple generic and certificate leaf [subject.CN] = \"Apple Mac OS Application Signing\"" as CFString,
+        return isApple
+    }()
+    
+    static var appStoreSecRequirement: SecRequirement? = {
+        var requirement: SecRequirement?
+        SecRequirementCreateWithString("anchor apple generic and certificate leaf [subject.CN] = \"Apple Mac OS Application Signing\"" as CFString,
                                        SecCSFlags(rawValue: 0),
-                                       &isAppStore)
-    _ = SecRequirementCreateWithString("anchor apple generic" as CFString,
-                                       SecCSFlags(rawValue: 0),
-                                       &isDeveloper)
+                                       &requirement)
+        return requirement
+    }()
 
-    if SecCodeCheckValidity(code, flags, isApple) == errSecSuccess {
-        return .apple
-    } else if SecCodeCheckValidity(code, flags, isAppStore) == errSecSuccess {
-        return .appstore
-    } else if SecCodeCheckValidity(code, flags, isDeveloper) == errSecSuccess {
-        return .developer
+    static var developerSecRequirement: SecRequirement? = {
+        var requirement: SecRequirement?
+        SecRequirementCreateWithString("anchor apple generic" as CFString,
+                                       SecCSFlags(rawValue: 0),
+                                       &requirement)
+        return requirement
+    }()
+
+    
+    static func extract(for pid: pid_t) -> SigningInfo? {
+        var status: OSStatus = -1
+        var dynamicCode: SecCode?
+        /*
+            Get SecCode for a running process
+         **/
+        status = SecCodeCopyGuestWithAttributes(nil,
+                                                [kSecGuestAttributePid : pid] as CFDictionary,
+                                                SecCSFlags([]),
+                                                &dynamicCode)
+        guard status == errSecSuccess, let unwrappedDynamicCode = dynamicCode else { return nil }
+        
+        /*
+            Perform dynamic validation of a SecCode
+         **/
+        status = SecCodeCheckValidity(unwrappedDynamicCode, SecCSFlags([]), nil)
+        guard status == errSecSuccess else { return nil }
+        
+        /*
+            Get code signer. It's not required, but we have to show something
+            when for example there is no code signing info with adHoc
+         **/
+        let codeSigner = extractCodeSigner(unwrappedDynamicCode, flags: SecCSFlags([]))
+        
+        /*
+            Cast dynamic code to static code
+         **/
+        var signingInfo: CFDictionary?
+        var staticCode: SecStaticCode?
+        status = SecCodeCopyStaticCode(unwrappedDynamicCode, SecCSFlags([]), &staticCode)
+        guard status == errSecSuccess, let unwrappedStaticCode = staticCode else { return nil }
+
+        /*
+            Finally get signing dictionary
+         **/
+        status = SecCodeCopySigningInformation(unwrappedStaticCode,
+                                               SecCSFlags(rawValue: kSecCSSigningInformation),
+                                               &signingInfo)
+        guard status == errSecSuccess,
+              let castedSigningInfo = signingInfo as? Dictionary<AnyHashable, Any>
+        else { return nil }
+
+        return SigningInfo(codeSigner: codeSigner,
+                           teamIdentifier: castedSigningInfo[kSecCodeInfoTeamIdentifier] as? String,
+                           signingIdentifier: castedSigningInfo[kSecCodeInfoIdentifier] as? String)
     }
-    return .adHoc
+    
+    static func extractCodeSigner(_ code: SecCode, flags: SecCSFlags) -> CodeSigner {
+        if SecCodeCheckValidity(code, flags, appleSecRequirement) == errSecSuccess { return .apple }
+        else if SecCodeCheckValidity(code, flags, appStoreSecRequirement) == errSecSuccess { return .appstore }
+        else if SecCodeCheckValidity(code, flags, developerSecRequirement) == errSecSuccess { return .developer }
+        return .adHoc
+    }
 }
