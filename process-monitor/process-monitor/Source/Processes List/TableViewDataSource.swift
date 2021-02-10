8df08a33016c7239f7a3cd666d74ed637e5f240d
Roman Kopaliani <romankopaliani@pingidentity.com>
2021-02-10 14:44:06 +0200

    Postpone it atm, time is running out

--- process-monitor/process-monitor/Source/Processes List/TableViewDataSource.swift
+++ process-monitor/process-monitor/Source/Processes List/TableViewDataSource.swift
@@ -5,9 +5,30 @@
 //  Created by Roman Kopaliani on 10.02.2021.
 //
 
-import Foundation
+import AppKit
 
 
-final class TableViewDataSource {
+protocol TableViewDataSourceDelegate {
+    associatedtype Cell: NSTableCellView
+    associatedtype Object
     
+    func configure(_ cell: Cell, with columnId: String, for object: Object)
+}
+
+protocol DataProvider {
+    associatedtype Object
+        
+    func object(at idx: Int) -> Object
+    func numberOfObjects() -> Int
+}
+
+protocol DataProviderDelegate {
+    func provider(_ provider: DataProvider, didChangeObject: Value)
+}
+
+final class TableViewDataSource<Delegate: TableViewDataSourceDelegate>: NSObject, NSTableViewDataSource {
+    typealias Object = Delegate.Object
+    typealias Cell = Delegate.Cell
+    
+//    required init(tableView: NSTableView, initialData: [Object], )
 }
