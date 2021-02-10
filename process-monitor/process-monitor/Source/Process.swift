11ca16e6c535f2d9ec7eeb9a30a1bec258510a84
Roman Kopaliani <romankopaliani@pingidentity.com>
2021-02-10 16:48:56 +0200

    Add initial SigningInfo structure. Needs clean up and refactoring.

--- process-monitor/process-monitor/Source/Process.swift
+++ process-monitor/process-monitor/Source/Process.swift
@@ -18,6 +18,8 @@ struct ProcessData {
     
     var bundleId: String?
     var certificateTeamId: String?
+    
+    var signingInfo: SigningInfo?
 }
 
 extension ProcessData: Hashable {}
@@ -37,12 +39,13 @@ extension ProcessData {
         self.bundleId = app.bundleIdentifier
         self.path = app.bundleURL?.absoluteString ?? ""
         self.certificateTeamId = "cert"
+        self.signingInfo = extractCodeSigningInfo(for: self.pid)
         print("ProcessID \(pid), user \(self.uid) bunldeId \(String(describing: self.bundleId)), path\(self.path)")
     }
 }
 
 
-fileprivate func fetchPid(for pid: pid_t) -> pid_t? {
+func fetchPid(for pid: pid_t) -> pid_t? {
     var kinfo = kinfo_proc()
     var size  = MemoryLayout<kinfo_proc>.stride
     var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
@@ -55,7 +58,7 @@ fileprivate func fetchPid(for pid: pid_t) -> pid_t? {
     return kinfo.kp_eproc.e_ppid
 }
 
-fileprivate func fetchUid(for pid: pid_t) -> uid_t? {
+func fetchUid(for pid: pid_t) -> uid_t? {
     var kinfo = kinfo_proc()
     var size  = MemoryLayout<kinfo_proc>.stride
     var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, pid]
@@ -68,30 +71,77 @@ fileprivate func fetchUid(for pid: pid_t) -> uid_t? {
     return kinfo.kp_eproc.e_ucred.cr_uid
 }
 
-//
-//fileprivate func fetchPath(for pid: pid_t) -> String? {
-//    // Allocate a buffer to store the name
-//      let nameBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(MAXPATHLEN))
-//      defer {
-//          nameBuffer.deallocate()
-//      }
-//
-//      // Now get and print the name. Not all processes return a name here...
-//      let nameLength = proc_name(pid, nameBuffer, UInt32(MAXPATHLEN))
-//      if nameLength > 0 {
-//          let name = String(cString: nameBuffer)
-//          print("  name=\(name)")
-//      }
-//
-//      // ...so also get the process' path
-//      let pathBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(MAXPATHLEN))
-//      defer {
-//          pathBuffer.deallocate()
-//      }
-//    let pathLength = Darwin.proc_pidpath(pid, pathBuffer, UInt32(MAXPATHLEN))
-//      if pathLength > 0 {
-//          let path = String(cString: pathBuffer)
-//          print("  path=\(path)")
-//      }
-//    return
-//}
+enum CodeSinger {
+    case apple
+    case appstore
+    case developer
+    case adHoc
+}
+
+struct SigningInfo: Hashable {
+    let signingStatus: OSStatus
+    let codeSigner: CodeSinger
+}
+
+func extractCodeSigningInfo(for pid: pid_t) -> SigningInfo? {
+    var status: OSStatus = -1
+    var dynamicCode: SecCode?
+    status = SecCodeCopyGuestWithAttributes(nil,
+                                            [kSecGuestAttributePid : pid] as CFDictionary,
+                                            SecCSFlags([]),
+                                            &dynamicCode)
+    guard status == errSecSuccess else { return nil }
+    guard let unwrappedDynamicCode = dynamicCode else { return nil }
+    
+    status = SecCodeCheckValidity(unwrappedDynamicCode, SecCSFlags([]), nil)
+    guard status == errSecSuccess else { return nil }
+    
+    let codeSigner = extractCodeSigner(unwrappedDynamicCode, flags: SecCSFlags([]))
+    
+    var signingInfo: CFDictionary?
+    var staticCode: SecStaticCode?
+    status = SecCodeCopyStaticCode(unwrappedDynamicCode, SecCSFlags([]), &staticCode)
+    guard status == errSecSuccess else { return nil }
+    guard let unwrappedStaticCode = staticCode else { return nil }
+
+    status = SecCodeCopySigningInformation(unwrappedStaticCode,
+                                           SecCSFlags(rawValue: kSecCSSigningInformation),
+                                           &signingInfo)
+    guard status == errSecSuccess else { return nil }
+    guard let castedSigningInfo = signingInfo as? Dictionary<AnyHashable, Any> else { return nil }
+    
+    if let teamIdentifier = castedSigningInfo[kSecCodeInfoTeamIdentifier] as? String {
+        print(teamIdentifier)
+    }
+
+    if let dentifier = castedSigningInfo[kSecCodeInfoIdentifier] as? String {
+        print(dentifier)
+    }
+
+    return SigningInfo(signingStatus: status, codeSigner: codeSigner)
+}
+
+func extractCodeSigner(_ code: SecCode, flags: SecCSFlags) -> CodeSinger {
+    var isApple: SecRequirement?
+    var isAppStore: SecRequirement?
+    var isDeveloper: SecRequirement?
+    
+    _ = SecRequirementCreateWithString("anchor apple" as CFString,
+                                       SecCSFlags(rawValue: 0),
+                                       &isApple)
+    _ = SecRequirementCreateWithString("anchor apple generic and certificate leaf [subject.CN] = \"Apple Mac OS Application Signing\"" as CFString,
+                                       SecCSFlags(rawValue: 0),
+                                       &isAppStore)
+    _ = SecRequirementCreateWithString("anchor apple generic" as CFString,
+                                       SecCSFlags(rawValue: 0),
+                                       &isDeveloper)
+
+    if SecCodeCheckValidity(code, flags, isApple) == errSecSuccess {
+        return .apple
+    } else if SecCodeCheckValidity(code, flags, isAppStore) == errSecSuccess {
+        return .appstore
+    } else if SecCodeCheckValidity(code, flags, isDeveloper) == errSecSuccess {
+        return .developer
+    }
+    return .adHoc
+}
